aeaa1e8e0407ceec8320df807255e7edd38b5c6b
The-element <nicke.jonsson@live.com>
2020-08-10 13:37:01 +0000

    added a shadow to the navbar that is shown on scroll

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,7 @@
 // When the user scrolls down 50px from the top of the document, resize the header's font size
 var screenSize = parseInt(window.screen.width);
 
+
 $(window).scroll(function() {
     if (screenSize > 767) {
         scrollFunction();
@@ -13,9 +14,11 @@ function scrollFunction() {
         document.getElementById("header").style.height = "60px";
         document.getElementById("header").style.fontSize = "14px";
         document.getElementById("logo-header").style.height = "60px";
+        document.getElementById("header").style.boxShadow = "0 0 20px rgba(0, 0, 0, 0.8)";
     } else {
         document.getElementById("header").style.height = "83px";
         document.getElementById("header").style.fontSize = "16px";
         document.getElementById("logo-header").style.height = "70px";
+        document.getElementById("header").style.boxShadow = "0 0";
     }
 }
